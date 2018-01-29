a968d5822cd08e3201990a9c3db5e7fe9422ff02
joemin <josephmin@gmail.com>
2018-01-28 22:55:02 -0500

    def need those scripts

--- scripts/restart_supervisord.sh
+++ scripts/restart_supervisord.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-/home/ec2-user/mrmincredible/venv/bin/supervisorctl reread
-/home/ec2-user/mrmincredible/venv/bin/supervisorctl update
+sudo /home/ec2-user/mrmincredible/venv/bin/supervisorctl reread
+sudo /home/ec2-user/mrmincredible/venv/bin/supervisorctl update
+sudo /home/ec2-user/mrmincredible/venv/bin/supervisorctl restart mrmincredible_flask_app
